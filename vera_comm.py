be97e5ddf925174cca818f9c275e8baad6391674
newpro <aaron.li1993@gmail.com>
2015-09-23 16:07:01 -0400

    add class and check wave status function

--- vera_comm.py
+++ vera_comm.py
@@ -7,7 +7,13 @@ from subprocess import Popen, PIPE
 import time
 from os import setsid
 
-def findip(vera_port):
+def find_vera(vera_port = 49451):
+    '''
+    this function give the vera ip in the network
+    this function is intended to call only once! 
+    Because Vera have delay, and sometime not even responed, that is why this function is not in class
+    Once find, vera IP do not change easily. unless rebot or others
+    '''
     timeout_time = 1
     while (timeout_time > 0): #if not find, increase scan time
         print "--------------start scanning for " + str(timeout_time)  +" secs----------------"
@@ -29,17 +35,39 @@ def findip(vera_port):
                     return vera_ip
         
         if timeout_time > 10: #more than 10 secs scan time, probably something goes wrong
-            raise Exception("CRITICAL: timeout quota reached, no Vera find")
+            raise Exception("CRITICAL: timeout quota reached, no Vera find\nVera do this sometime, u might want to try later.")
 
 #test       
-vera_port = 49451
 #vera_ip = findip(vera_port)
-vera_ip = "192.168.0.100"
+
 
 #_______________________parse UML data_________________________
 import requests
 import xml.etree.ElementTree as ET
-r = requests.get("http://" + str(vera_ip) + ":" + str(vera_port) + "/data_request?id=lu_status")
-print type(r.json())
+class ChilVera:
+    #hardcode info
+    def __init__(self, vera_ip = "192.168.0.100", vera_port = 49451):
+        self.vera_ip = vera_ip
+        self.vera_port = vera_port
+        self.resp = requests.get("http://" + str(vera_ip) + ":" + str(vera_port) + "/data_request?id=lu_status").json()
+    
+    def update_vera_status(self, vera_ip, vera_port):
+        """
+        get status information of all vera devices in system
+        return: unparsed json file, and parse
+        """
+        self.resp = requests.get("http://" + str(self.vera_ip) + ":" + str(self.vera_port) + "/data_request?id=lu_status").json()
+    
+    def get_wave_status(self, update = False):
+        '''
+        check if the wave is alive
+        Option: -update: if true then repoll the status
+        return: 
+        '''
+        if(update == True):
+            update_vera_status()
+        return self.resp["ZWaveStatus"]
 
-
+#test
+myvera = ChilVera()
+print myvera.get_wave_status()
